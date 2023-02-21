867d0cfc2263cb98cdc2f690b143dbda28f3671a
aishirin <awrnjoomartova@gmail.com>
2023-02-21 16:13:56 +0600

    Finished1

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,15 +1,31 @@
 import "./Slider.scss";
-import { useState } from "react";
-import { useEffect } from "react";
+// import { useState } from "react";
+// import { useEffect } from "react";
 
 export default function Slider({ openModal }) {
-  const [photos, setPhotos] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:3001/slider")
-      .then((res) => res.json())
-      .then((data) => setPhotos(data));
-  }, []);
-  console.log(photos);
+  const slider=[
+    {
+      "title": "Aishirin1",
+      "id": 1,
+      "images": "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg"
+    },
+    {
+      "title": "Aishirin11",
+      "id": 2,
+      "images": "https://www.thebalancemoney.com/thmb/Ci8l5i-cl3_emjyarN7RVunlULo=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/enjoying-his-studies-484286872-735782e8d2144f7496bf9b947bcc1a56.jpg"
+    },
+    {
+      "title": "Aishirin22",
+      "id": 3,
+      "images": "https://www.cdc.gov/healthyyouth/whatworks/images/GettyImages-492699342.jpg?_=83260"
+    }
+  ]
+  // const [photos, setPhotos] = useState([]);
+  // useEffect(() => {
+  //   fetch("http://localhost:3001/slider")
+  //     .then((res) => res.json())
+  //     .then((data) => setPhotos(data));
+  // }, []);
 
   function randomId(max) {
     return Math.floor(Math.random() * max);
@@ -20,7 +36,7 @@ export default function Slider({ openModal }) {
     <div className="slider">
       <div>
         <img alt="some"
-          src={photos[randomId(3)] && photos[randomId(3)].images}
+          src={slider[randomId(3)] && slider[randomId(3)].images}
           className="img"
         ></img>
       </div>
