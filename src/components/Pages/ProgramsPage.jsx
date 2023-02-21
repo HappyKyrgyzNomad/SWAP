2ececb65cd197524c536223a16e8630ac36155e2
HappyKyrgyzNomad <kanatbeks.son@gmail.com>
2023-02-21 16:06:02 +0600

    Fixed

--- src/components/Pages/ProgramsPage.jsx
+++ src/components/Pages/ProgramsPage.jsx
@@ -1,11 +1,10 @@
 import Programs from "../Programs/Programs";
-import SearchBar from "../SearchBar/SearchBar";
+
 import "./ProgramsPage.scss";
 
 export default function ProgramsPage() {
   return (
     <div className="parent">
-      <SearchBar className="parent__bar" />
       <div className="parent__programs">
         <Programs />
       </div>
